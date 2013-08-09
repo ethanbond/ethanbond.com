207dc3577427af2d25029ae9af379b7a2302fd6d
Ethan Bond <ethan@emberi.co>
2013-08-08 20:00:42 -0400

    slight changes

--- hook.sh
+++
@@ -1,9 +0,0 @@
-cd /hookstaging && wget -O website.zip -q https://github.com/ethanbond/ethanbond.com/archive/master.zip
-
-if [ -f /hookstaging/website.zip ]; then
-	unzip -q /hookstaging/website.zip
-	rm /hookstaging/website.zip
-	mv Project-master ethanbond.com
-	rm -rf ~/ethanbond.com
-	mv ethanbond.com ~/ethanbond.com
-fi
